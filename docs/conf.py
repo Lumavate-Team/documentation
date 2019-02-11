e6b42630b41b47062b2f09bdd799d34bce80e140
Emily McLaughlin <45008039+mclaughline@users.noreply.github.com>
2019-02-11 15:40:13 -0500

    Update conf.py
    See if this fixes our problem.

--- docs/conf.py
+++ docs/conf.py
@@ -104,6 +104,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
+    'display_version': False
     'sticky_navigation': True,
     'includehidden': True,
     'collapse_navigation': False
@@ -140,7 +141,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-# html_last_updated_fmt = '%m, %Y'
+ html_last_updated_fmt = '%m, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -163,7 +164,7 @@ html_static_path = ['_static']
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
