cc36819cf8d2fb0bbd7e382947fa9f744b54adff
Emily McLaughlin <45008039+mclaughline@users.noreply.github.com>
2019-02-12 11:56:47 -0500

    Update conf.py
    I am trying an absolute route.

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys
 import os
 
 def setup(app):
-    app.add_stylesheet('_templates/custom.css')
+    app.add_stylesheet('https://github.com/Lumavate-Team/documentation/docs/_templates/custom.css')
     
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
