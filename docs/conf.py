c9889261a0c0d876178017b5a617c1e517c0cdef
Tim Butler <butlertg@gmail.com>
2019-02-12 09:07:03 -0500

    Update conf.py
    Update latex commands to use overview page vs. index

--- docs/conf.py
+++ docs/conf.py
@@ -204,7 +204,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
+  ('1-overview', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
    u'Read the Docs', 'manual'),
 ]
 
@@ -234,7 +234,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
+    ('1-overview', 'readthedocstemplate', u'Read the Docs Template Documentation',
      [u'Read the Docs'], 1)
 ]
 
@@ -248,7 +248,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
+  ('1-overview', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
    u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
    'Miscellaneous'),
 ]
