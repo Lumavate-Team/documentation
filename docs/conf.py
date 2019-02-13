cd0a416ad0fcaa7a718cc12a699ed7d699296131
Emily McLaughlin <45008039+mclaughline@users.noreply.github.com>
2019-02-13 10:04:04 -0500

    Update conf.py
    Might have just found my fix.

--- docs/conf.py
+++ docs/conf.py
@@ -119,10 +119,10 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'Lumavate Developer Portal'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = 'Lumavate Dev Portal'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -186,11 +186,6 @@ html_show_copyright = True
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'ReadtheDocsTemplatedoc'
-
-html_context = {
-"last_updated": True,
-"commit": False,
-}
 
 # -- Options for LaTeX output ---------------------------------------------
 
