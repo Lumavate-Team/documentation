54198ddb6d614aa9970f4551607fd1df82aa690d
Emily McLaughlin <45008039+mclaughline@users.noreply.github.com>
2019-02-18 14:32:12 -0500

    Update conf.py
    Setup some extensions.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,9 @@ def setup(app):
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.githubpages', 'sphinx.ext.ifconfig', 'sphinx.ext.intersphinx']
+
+intersphinx_mapping = {}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
